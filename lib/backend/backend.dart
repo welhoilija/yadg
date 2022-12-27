1704471b92e65fe81a65e8a03595e4f010b80fa7
Welhoilija <=>
2022-12-27 17:32:48 +0200

    updated backend and dart

--- lib/backend/backend.dart
+++ lib/backend/backend.dart
@@ -2,61 +2,42 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
-import 'package:yadg/backend/card.dart';
+import 'card.dart';
+import 'player.dart';
 
 void main() {
   Backend backend = Backend(
       [Player("BÄLE"), Player("HANKI"), Player("TUPE"), Player("IIGGAZ")]);
-  final cards = backend.fetchCards();
-  print(backend.getRandomCard(cards, backend.players));
+  print(backend.getRandomCard(backend.players));
 }
 
 class Backend {
   // Add players with this
-  Backend(this.players);
-  List<Player> players;
+
+  late List<Player> players;
+  late List<dynamic> cards;
+  Backend(this.players) {
+    fetchCards();
+  }
 
   // Path to the JSON file
   final String _path = './cards.json';
 
   // Function to fetch the JSON file
-  List<dynamic> fetchCards() {
+  void fetchCards() {
     final file = File(_path);
     if (file.existsSync()) {
       final contents = file.readAsStringSync();
-      return jsonDecode(contents);
+      cards = jsonDecode(contents);
     } else {
       throw Exception('Cards file not found');
     }
   }
 
-  String getRandomCard(List<dynamic> json, List<Player> players) {
+  String getRandomCard(List<Player> players) {
     final rand = Random();
-    final card = Card(json[rand.nextInt(json.length)], players);
+    final card = Card(cards[rand.nextInt(cards.length)], players);
 
     return card.getFormattedText();
   }
-
-  // Function to return a list of text items from the JSON file
-  List<dynamic> getTextCards() {
-    final cards = fetchCards();
-    return cards.map((card) => card['text']).toList();
-  }
-
-  // Function to return a list of emoji items from the JSON file
-  List<dynamic> getEmojiCards() {
-    final cards = fetchCards();
-    return cards.map((card) => card['emoji']).toList();
-  }
-}
-
-class Player {
-  final String name;
-  int failedCards = 0;
-  int successfulCards = 0;
-
-  Player(this.name);
-
-  void failCard() => failedCards++;
-  void succeedCard() => successfulCards++;
 }
--- lib/backend/card.dart
+++ lib/backend/card.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import 'package:yadg/backend/backend.dart';
+import 'player.dart';
 
 class Card {
   final rand = Random();
@@ -15,13 +15,19 @@ class Card {
     text = json['text'];
 
     player1 = players[rand.nextInt(players.length)];
-    player2 = rand.nextBool() ? players[rand.nextInt(players.length)] : null;
+    player2 = players[rand.nextInt(players.length)];
+
+    if (player2 != null && player1 == player2) {
+      player2 = players[(players.indexOf(player1) + 1) % players.length];
+    }
   }
 
   String getFormattedText() {
     String modifiedText = text.replaceAll('{player}', player1.name);
-    if (player2 != null) {
-      modifiedText = modifiedText.replaceAll('{player}', player2!.name);
+    if (modifiedText.contains('{player2}')) {
+      if (player2 != null) {
+        modifiedText = modifiedText.replaceAll('{player2}', player2!.name);
+      }
     }
 
     return modifiedText.replaceAll('{amount}', rand.nextInt(5).toString());
